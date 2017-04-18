1af55ddc800fe811eb0ee1574e110dda525f2a0d
lenangungu <lenangungu@hotmail.co.uk>
2017-04-17 23:16:05 -0500

    Gui head implemented

--- robot.cpp
+++ robot.cpp
@@ -1,10 +1,434 @@
-#include <iostream>
+
 #include <vector>
-#include "robot_part.h"
-#include "utility.h"
+
+//#include "utility.h"
+#include <climits>
+#include <exception>
+#include <FL/Fl.H>
+#include <FL/fl_ask.H>
+#include <FL/Fl_Input.H>
+#include <cstdlib>
+#include <FL/Fl_Window.H>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
+class Robot_part {
+  public:
+    Robot_part(string description, double cost, int model_number, string name)
+        : _description{description}, _cost{cost}, _model_number{model_number}, _name{name} { }
+    string name() const {return _name;}
+    int model_number() const {return _model_number;}
+    double cost() const {return _cost;}
+    string description() const {return _description;}
+  protected:
+    string _name;
+    int _model_number;
+    double _cost;
+    string _description;
+};
+
+class Head : public Robot_part {
+  public:
+    Head(string name,
+         int model_number,
+         double cost,
+         string description,
+         double power)
+       : Robot_part(description, cost, model_number, name),
+         _power{power} { }
+/*
+    Head override (ifstream& ist){
+    (ist.open("robot.txt"));
+    ist >> _name; ist >> _model_number; ist >> _cost; ist >> _description; ist>> _power;
+    Head(_name,_model_number,_cost,_description,_power);
+    }*/
+
+    double power() const {return _power;}
+    void save(ofstream& ost)
+    { ost.open("robot.txt",ios_base::app);
+    ost << "2"<<endl; ost <<"1"<<endl; ost<<_name << endl; ost << _model_number << endl; ost << _cost << endl;ost << _description << endl; ost << _power << endl;
+    ost.close();
+    }
+
+  private:
+    double _power;
+};
+
+ostream& operator<< (ostream& ost, const Head& head) {
+  ost << "Head " << head.name() << " (model " << head.model_number() << ") "
+      << "costs $" << head.cost()
+      << " and uses " << head.power() << " watts" << endl;
+  ost << head.description();
+  return ost;
+}
+
+class Locomotor : public Robot_part {
+  public:
+    Locomotor(string name,
+              int model_number,
+              double cost,
+              string description,
+              double max_power)
+            : Robot_part(description, cost, model_number, name),
+              _max_power{max_power} { }
+    double max_power() const {return _max_power;}
+   void save(ofstream& ost)
+    { ost.open("robot.txt",ios_base::app);
+    ost << "2"<<endl; ost <<"4"<<endl;ost << _name << endl; ost << _model_number << endl; ost << _cost << endl;ost << _description << endl; ost << _max_power << endl;
+    ost.close();
+    }
+  private:
+    double _max_power;
+};
+
+ostream& operator<< (ostream& ost, const Locomotor& locomotor) {
+  ost << "Locomotor " << locomotor.name() << " (model " << locomotor.model_number() << ") "
+      << "costs $" << locomotor.cost()
+      << " and uses up to " << locomotor.max_power() << " watts" << endl;
+  ost << locomotor.description();
+  return ost;
+}
+
+class Arm : public Robot_part {
+  public:
+    Arm(string name,
+        int model_number,
+        double cost,
+        string description,
+        double max_power)
+      : Robot_part(description, cost, model_number, name),
+        _max_power{max_power} { }
+    double max_power() const {return _max_power;}
+   void save(ofstream& ost)
+    { ost.open("robot.txt",ios_base::app);
+    ost << "2"<<endl; ost <<"2"<<endl;ost << _name << endl; ost << _model_number << endl; ost << _cost << endl;ost << _description << endl; ost << _max_power << endl;
+    ost.close();
+    }
+  private:
+    double _max_power;
+};
+
+ostream& operator<< (ostream& ost, const Arm& arm) {
+  ost << "Arm " << arm.name() << " (model " << arm.model_number() << ") "
+      << "costs $" << arm.cost()
+      << " and uses up to " << arm.max_power() << " watts" << endl;
+  ost << arm.description();
+  return ost;
+}
+
+class Torso : public Robot_part {
+  public:
+    Torso(string name,
+          int model_number,
+          double cost,
+          string description,
+          int max_arms,
+          int battery_compartments)
+        : Robot_part(description, cost, model_number, name),
+          _max_arms{max_arms},
+          _battery_compartments{battery_compartments} { }
+    int max_arms() const {return _max_arms;}
+    int battery_compartments() const {return _battery_compartments;}
+  void save(ofstream& ost)
+    { ost.open("robot.txt",ios_base::app);
+    ost << "2"<<endl; ost <<"3"<<endl;ost << _name << endl; ost << _model_number << endl; ost << _cost << endl;ost << _description << endl; ost << _max_arms << endl; ost << _battery_compartments << endl;
+    ost.close();
+    }
+
+  private:
+    int _max_arms;
+    int _battery_compartments;
+};
+
+ostream& operator<< (ostream& ost, const Torso& torso) {
+  ost << "Torso " << torso.name() << " (model " << torso.model_number() << ") "
+      << "costs $" << torso.cost()
+      << " and can hold " << torso.max_arms() << " arms and "
+      << torso.battery_compartments() << " batteries" << endl;
+  ost << torso.description();
+  return ost;
+}
+
+class Battery : public Robot_part {
+  public:
+    Battery(string name,
+            int model_number,
+            double cost,
+            string description,
+            double max_energy,
+            double power_available)
+          : Robot_part(description, cost, model_number, name),
+            _max_energy{max_energy},
+            _power_available{power_available} { }
+    double max_energy() const {return _max_energy;}
+    double power_available() const {return _power_available;}
+  void save(ofstream& ost)
+    { ost.open("robot.txt",ios_base::app);
+    ost << "2"<<endl; ost <<"5"<<endl;ost << _name << endl; ost << _model_number << endl; ost << _cost << endl;ost << _description << endl; ost << _max_energy << endl; ost <<_power_available <<endl;
+    ost.close();
+    }
+
+  private:
+    double _max_energy;
+    double _power_available;
+};
+
+ostream& operator<< (ostream& ost, const Battery& battery) {
+  ost << "Battery " << battery.name() << " (model " << battery.model_number() << ") "
+      << "costs $" << battery.cost()
+      << ", driving " << battery.power_available() << " watts "
+      << "and containing " << battery.max_energy() << " kWh"  << endl;
+  ost << battery.description();
+  return ost;
+}
+
+class Robot_model {
+  public:
+    Robot_model(string name, int model_number, Head head, Torso torso, Arm arm , Locomotor locomotor, Battery battery)
+        : _name{name}, _model_number{model_number}, _head{head}, _torso{torso},_arm{arm},_locomotor{locomotor},_battery{battery} { }
+    string name() const {return _name;}
+    int model_number() const {return _model_number;}
+    void head() const{ cout << _head << endl;cout << endl;}
+    void arm() const{cout << _arm << endl; cout << endl;}
+    void torso() const{cout << _torso << endl; cout << endl;}
+    void locomotor() const{cout << _locomotor<< endl; cout << endl;}
+    void battery() const{cout << _battery << endl; cout << endl;}
+    double cost() const { double total_cost = _head.cost() + _arm.cost() + _torso.cost() + _locomotor.cost() + _battery.cost(); return total_cost;
+}
+
+void save(ofstream& ost)
+{
+ost.open("robot.txt",ios_base::app);
+ost << "1"<<endl; ost << _name << endl; ost << _model_number << endl; ost << _head <<endl; ost << _torso << endl; ost << _arm << endl; ost << _locomotor << endl; ost << _battery << endl;
+}
+  protected:
+    string _name;
+    int _model_number;
+    Head _head;
+    Arm _arm;
+    Torso _torso;
+    Locomotor _locomotor;
+    Battery _battery;
+};
+
+
+ostream& operator<< (ostream& ost, const Robot_model& model) {
+  ost << "Robot Model " << model.name() << " (model " << model.model_number() << ") "
+     << "With parts: " <<  endl;
+     model.head();
+     model.arm();
+     model.torso();
+     model.locomotor();
+     model.battery();
+
+  ost << "Total cost of the model is: $"<< model.cost() << endl;
+
+
+  return ost;
+}
+
+class Customer
+{
+private:
+	string _name;
+	int _customer_number;
+	string _phone_number;
+	string _email_address;
+
+public:
+	Customer(string name, int customer_number,string phone_number,string email_address) : _name{name},_customer_number{customer_number},_phone_number{phone_number},_email_address{email_address} {}
+    string name() const { return _name;}
+    int customer_number() const {return _customer_number;}
+    string phone_number() const {return _phone_number;}
+    string email_address() const {return _email_address;}
+
+    void save(ofstream& ost)
+    {
+    ost << "4" << endl;ost << _name << endl; ost << _customer_number << endl; ost << _phone_number << endl; ost << _email_address << endl;
+    }
+
+};
+ostream& operator<< (ostream& ost, const Customer& customer) {
+  ost << "Customer " << customer.name() << " ( " <<customer.customer_number() << ") "
+     << "Phone: " << customer.phone_number() << " Email: " << customer.email_address() << endl;
+
+  return ost;
+}
+
+class Sale_associate
+{
+private:
+	string _name;
+	int _employee_number;
+
+public:
+	Sale_associate(string name, int employee_number): _name{name},_employee_number{employee_number}{}
+
+    void save(ofstream& ost)
+    {
+    ost << "5"<< endl;ost << _name << endl; ost << _employee_number << endl;
+    }
+
+    string name() const {return _name;}
+    int employee_number() const {return _employee_number;}
+};
+
+
+ostream& operator<< (ostream& ost, const Sale_associate& sale_associate) {
+  ost << "Sale associate " << sale_associate.name() << " Employee n* " << sale_associate.employee_number() << endl;
+
+  return ost;
+}
+
+
+class Order
+{
+private:
+	int _order_number;
+	string _date;
+	Customer _customer;
+	Sale_associate _sale_associate;
+	Robot_model _model;
+	int _status;
+
+
+public:
+	Order(int order_number,string date,Customer customer,Sale_associate sale_associate,Robot_model model,int status) : _order_number{order_number},_date{date},_customer{customer},_sale_associate{sale_associate},_model{model},_status{status} {}
+
+	double robot_cost() {return _model.cost();}
+	double extended_price() {return _model.cost() * _order_number;}
+
+    int order_number() const {return _order_number;}
+    string date() const {return _date;}
+    Customer customer() const {return _customer;}
+    Sale_associate sale_associate() const {return _sale_associate;}
+    Robot_model model() const {return _model;}
+    int status() const {return _status;}
+
+    void save(ofstream& ost)
+    {
+    ost <<"6"<<endl;ost << _order_number << endl; ost << _date << endl; ost << _customer << endl; ost << _sale_associate << endl; ost << _model << endl; ost << _status << endl;
+    }
+
+};
+
+ostream& operator<< (ostream& ost, const Order& order) {
+  ost << "Order placed on " << order.date() << " By " << order.customer() << endl;
+
+  return ost;
+}
+//
+string get_string(string prompt) {
+  string result = "";
+  cout << prompt;
+  getline(cin, result);
+  return result;
+}
+
+//
+// Skip all characters in the STDIN buffer up to and including the next newline
+// For use after a cin stream operation, to clear invalid text and the newline
+//
+void skip() {
+  if (cin.fail())
+    cin.clear();
+  cin.ignore(INT_MAX, '\n');
+}
+
+//
+// Return any valid integer from the user
+//
+int get_int(string prompt) {
+  int result = 0;
+  while (true) {
+    cout << prompt;
+    cin >> result;
+    bool success = cin.good();
+    skip();
+    if (success) return result;
+    cerr << "### Not an integer!" << endl;
+  }
+}
+
+//
+// Return any valid integer between min_int and max_int, inclusive
+//
+int get_int(string prompt, int min_int, int max_int) {
+  int result = min_int - 1;
+  while (true) {
+    result = get_int(prompt);
+    if (min_int <= result && result <= max_int) return result;
+    cerr << "### Integer must be between " << min_int << " and " << max_int << "!\n";
+  }
+}
+
+//
+// Return any valid double from the user
+//
+double get_double(string prompt) {
+  double result = 0.0;
+  while (true) {
+    cout << prompt;
+    cin >> result;
+    bool success = cin.good();
+    skip();
+    if (success) return result;
+    cerr << "### Not a number!" << endl;
+  }
+}
+
+// /////////////////////////////////////////
+// F I L E   I / O
+// /////////////////////////////////////////
+
+//
+// Return any string from the file, including a simple \n
+//
+string get_string(istream& ist) {
+  string result = "";
+  getline(ist, result);
+  return result;
+}
+
+//
+// Skip all characters in the input buffer up to and including the next newline
+// For use after a input stream operation, to clear invalid text and the newline
+//
+void skip(istream& ist) {
+  if (ist.fail())
+    ist.clear();
+  ist.ignore(INT_MAX, '\n');
+}
+
+//
+// Return any valid integer from the file
+//
+int get_int(istream& ist) {
+  int result = 0;
+  ist >> result;
+  skip(ist);
+  return result;
+}
+
+//
+// Return any valid double from the file
+//
+double get_double(istream& ist) {
+  double result = 0.0;
+  ist >> result;
+  skip(ist);
+  return result;
+}
+
+
 int main() {
+
+ Fl_Window *win = new Fl_Window(600,500, "Robot Shop");
+ win->color(FL_WHITE);
+win->show();
 
   Head *head;
   Arm *arm;
@@ -16,20 +440,7 @@ int main() {
   Sale_associate *sale_associate;
   Order *order;
 
-/*
-  while(true) {
-    switch (get_int(R"(
-   Testing Robot Parts
 
-(1) Create Head
-(2) Create Arm
-(3) Create Torso
-(4) Create Locomotor
-(5) Create Battery
-(0) Exit
-
-Selection? )", 0, 5)) {
-*/
 vector <Robot_model> models;
 vector<Head> heads;
 vector<Torso> torsos;
@@ -42,7 +453,7 @@ vector<Order> orders;
 ofstream ost;
 ifstream ist;
 
-int option;
+
 int part;
 string model_name;
 int model_number;
@@ -133,14 +544,20 @@ break;}
 }
 }while(!ist.eof());
 ist.close();
+//fl_message("%s","1) Create a model '\n'2)Create parts'\n'3)List models'\n'4)Create a customer'\n'5)Create a sale associate'\n'6)Create an order'\n'9)Exit");
+
+string str = fl_input("1) Create a model \n2) Create parts\n3) List models\n4) Create a customer\n5) Create a sale associate\n6) Create an order\n9) Exit","");
+
+    int option = atoi(str.c_str());
 
 
-
+/*
 do
 {
 cout << "1) Create a Model" << endl << "2) Create parts" << endl << "3) List models" << endl << "4) Create a customer"<< endl<< "5) Create a sale associate" << endl<< "6) Create an order" <<endl<<"9) to exit" << endl;
 cin >> option;
 cin.ignore();
+*/
 
 switch (option)
 {
@@ -231,13 +648,15 @@ break;
 
 case 2:
 {
-int part;
-cout << endl;
-cout << "1) Head" << endl << "2) Arm " << endl << "3) Torso" << endl << "4) Locomotor" << endl << "5) Battery" << endl;
-cout << endl;
-cout << "What part do you want to create and add to the model? 0 to exit or to create another model" << endl;
-cin >> part;
-cin.ignore();
+
+
+//fl_message("%s","1) Head \n2) Arm\n3) Torso\n4) Locomotor\n5)Battery\n");
+
+string str = fl_input("1) Head \n2) Arm\n3) Torso\n4) Locomotor\n5)Battery\n","");
+
+    int part = atoi(str.c_str());
+
+
 switch (part)
 {
 do{
@@ -246,62 +665,15 @@ do{
         exit(0);
       case 1: // Head
         head = new Head{
-          get_string("Enter this head's name: "),
-          get_int("Enter this head's model number: "),
-          get_double("Enter this head's cost: "),
-          get_string("Enter description: "),
-          get_double("Enter this head's power: ")
+
+          fl_input("Head's name: ",""),atoi(fl_input("Head's model number: ","")),atof(fl_input("Head's cost: ","")),fl_input("Head's description: ",""),atof(fl_input("Head's power: ",""))
+
         };
         cout << *head << endl;
         head->save(ost);
         heads.push_back(*head);
         break;
-      case 2:
-        arm = new Arm{
-          get_string("Enter this arm's name: "),
-          get_int("Enter this arm's model number: "),
-          get_double("Enter this arm's cost: "),
-          get_string("Enter description: "),
-          get_double("Enter maximum power: ")
-        };
-        cout << *arm << endl;
-        arm->save(ost);
-        arms.push_back(*arm);
-        break;
-      case 3:
-        torso = new Torso{
-          get_string("Enter this torso's name: "),
-          get_int("Enter this torso's model number: "),
-          get_double("Enter this torso's cost: "),
-          get_string("Enter description: "),
-          get_int("Enter maximum # of arms (0 to 2): ", 0, 2),
-          get_int("Enter maximum # of batteries (1 to 3): ", 1, 3)
-        };
-        cout << *torso << endl;
-        torso->save(ost);
-        torsos.push_back(*torso);
-        break;
-      case 4:
-        locomotor = new Locomotor{
-          get_string("Enter this locomotor's name: "),
-          get_int("Enter this locomotor's model number: "),
-          get_double("Enter this locomotor's cost: "),
-          get_string("Enter description: "),
-          get_double("Enter maximum power: ")
-        };
-        cout << *locomotor << endl;
-        locomotor->save(ost);
-        locomotors.push_back(*locomotor);
-        break;
-      case 5:
-        battery = new Battery{
-          get_string("Enter this battery's name: "),
-          get_int("Enter this battery's model number: "),
-          get_double("Enter this battery's cost: "),
-          get_string("Enter description: "),
-          get_double("Enter maximum energy: "),
-          get_double("Enter power available: ")
-        };
+
         cout << *battery << endl;
         battery->save(ost);
         batteries.push_back(*battery);
@@ -435,5 +807,13 @@ break;
 }
 
 }
-  }while(option != 9);
+
+win->end();
+ // }while(option != 9);
 }
+
+
+
+// g++ -std=c++0x robot.cpp -lfltk -o robotgui
+//./robotgui
+
